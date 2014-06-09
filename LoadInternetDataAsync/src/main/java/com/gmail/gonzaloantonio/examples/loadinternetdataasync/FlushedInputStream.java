88e1cacd7a8c135a86d20d73f6a1eb8e524e9110
gonzososa <gonzasosa@outlook.com>
2014-06-09 13:56:21 -0500

    Download images in background working!

---
+++ LoadInternetDataAsync/src/main/java/com/gmail/gonzaloantonio/examples/loadinternetdataasync/FlushedInputStream.java
@@ -0,0 +1,34 @@
+package com.gmail.gonzaloantonio.examples.loadinternetdataasync;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+class FlushedInputStream extends FilterInputStream {
+
+    public FlushedInputStream (InputStream inputStream) {
+        super (inputStream);
+    }
+
+    @Override
+    public long skip (long n) throws IOException {
+        long totalBytesSkipped = 0L;
+
+        while (totalBytesSkipped < n) {
+            long bytesSkipped = in.skip (n - totalBytesSkipped);
+            if (bytesSkipped == 0L) {
+                int b = read ();
+                if (b < 0) {
+                    break;
+                }
+                else {
+                    bytesSkipped = 1;
+                }
+            }
+            totalBytesSkipped += bytesSkipped;
+        }
+
+        return totalBytesSkipped;
+    }
+
+}
--- LoadInternetDataAsync/src/main/java/com/gmail/gonzaloantonio/examples/loadinternetdataasync/MainActivity.java
+++ LoadInternetDataAsync/src/main/java/com/gmail/gonzaloantonio/examples/loadinternetdataasync/MainActivity.java
@@ -1,19 +1,16 @@
 package com.gmail.gonzaloantonio.examples.loadinternetdataasync;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.net.http.AndroidHttpClient;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.util.LruCache;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -23,32 +20,53 @@ import android.widget.ListView;
 
 import com.jakewharton.disklrucache.DiskLruCache;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends Activity {
     //private ImageView img;
     private String [] urls = {
-            "https://pbs.twimg.com/media/BkovgPLIAAA58ZL.jpg",
-            "https://pbs.twimg.com/media/BnCzJNgIUAAIsiJ.jpg",
-            "https://pbs.twimg.com/media/Bmv_RAfIMAAm0aa.jpg",
-            "https://pbs.twimg.com/media/BnJKb4_CYAEVtV9.jpg",
-            "https://pbs.twimg.com/media/BnPrWI3CEAAIzSP.jpg",
-            "https://pbs.twimg.com/media/BnEJAzoCIAAT4ZD.jpg",
-            "https://pbs.twimg.com/media/BnJqdCKCMAASTxj.jpg",
-            "https://pbs.twimg.com/media/Bm4ll80IQAAp9qP.jpg",
-            "https://pbs.twimg.com/media/Bm4N-biIAAA1o57.jpg"
+            "https://pbs.twimg.com/profile_images/462981550003740672/Jb-UpOux.jpeg",
+            "https://pbs.twimg.com/media/BoHECEXIgAEwrDi.jpg:large",
+            "https://pbs.twimg.com/media/BnemgXxCIAA-aif.jpg:large",
+            "https://pbs.twimg.com/media/BnXGVKbCAAA4LoV.jpg:large",
+            "https://pbs.twimg.com/media/BcMu4KhIMAAJSqC.jpg:large",
+            "https://pbs.twimg.com/media/Bb9qAagIgAAfSvg.jpg:large",
+            "https://pbs.twimg.com/media/BoU_qgLCMAAnAai.jpg:large",
+            "https://pbs.twimg.com/media/Bo0_K-2CcAArf_4.jpg:large",
+            "https://pbs.twimg.com/media/BjBZ9RLIIAEmDJ1.jpg:large",
+            "https://pbs.twimg.com/media/BkovgPLIAAA58ZL.jpg:large",
+            "https://pbs.twimg.com/media/BklXhMBIIAAAlvx.jpg:large",
+            "https://pbs.twimg.com/media/Bkjq2N4IEAE335P.jpg:large",
+            "https://pbs.twimg.com/media/Bka0wlkIIAAn1k8.jpg:large",
+            "https://pbs.twimg.com/media/BkaETdSCQAAfb7G.jpg:large",
+            "https://pbs.twimg.com/media/BkQFryrIcAAiodL.jpg:large",
+            "https://pbs.twimg.com/media/BkOpckpCcAA_70R.jpg:large",
+            "https://pbs.twimg.com/media/BpkWF6gIcAAxM6l.jpg:large",
+            "https://pbs.twimg.com/media/BpkKnBXIEAAHFDp.jpg:large",
+            "https://pbs.twimg.com/media/BpkJvfCIAAILJc-.jpg:large",
+            "https://pbs.twimg.com/media/BkOcfdJCcAEH0MG.jpg:large",
+            "https://pbs.twimg.com/media/BkEoEADIEAA3obl.jpg:large",
+            "https://pbs.twimg.com/media/Bj_tyNaIIAAqOp7.jpg:large",
+            "https://pbs.twimg.com/media/Bj1dPJqCAAA8m2Y.jpg:large",
+            "https://pbs.twimg.com/media/Bjwes9ICYAA7u-H.png:large",
+            "https://pbs.twimg.com/media/BjwUBCICAAAFS0m.jpg:large",
+            "https://pbs.twimg.com/media/BkovgPLIAAA58ZL.jpg:large",
+            "https://pbs.twimg.com/media/BpabvckCQAAT4ll.jpg:large",
+            "https://pbs.twimg.com/media/BnCzJNgIUAAIsiJ.jpg:large",
+            "https://pbs.twimg.com/media/BppFs51IAAAQ60s.jpg:large",
+            "https://pbs.twimg.com/media/BpkYt8DCAAAmaZ8.jpg:large",
+            "https://pbs.twimg.com/media/BnPrWI3CEAAIzSP.jpg:large",
+            "https://pbs.twimg.com/media/BnEJAzoCIAAT4ZD.jpg:large",
+            "https://pbs.twimg.com/media/BnJqdCKCMAASTxj.jpg:large",
+            "https://pbs.twimg.com/media/Bm4ll80IQAAp9qP.jpg:large",
+            "https://pbs.twimg.com/media/Bm4N-biIAAA1o57.jpg:large"
     };
 
     private LruCache<String, Bitmap> memCache;
@@ -75,48 +93,76 @@ public class MainActivity extends Activity {
 
 
         Button button = (Button) findViewById (R.id.button1);
-        button.setOnClickListener (new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            listView1.setAdapter (
-                new ArrayAdapter<String> (MainActivity.this, R.id.list_item, urls) {
-                    @Override
-                    public View getView (int position, View convertView, ViewGroup parent) {
-                        ImageView img;
+            listView1.setAdapter (new ArrayAdapter<String>(MainActivity.this, R.id.list_item, urls) {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    ImageView img;
 
-                        if (convertView == null) {
-                            img = new ImageView (parent.getContext ());
-                            img.setMinimumHeight (200);
-                            img.setPadding (7, 7, 7, 7);
-                        } else {
-                            img = (ImageView) convertView;
-                        }
-
-                        /*LayoutInflater inflater = (LayoutInflater) getContext().getSystemService (Context.LAYOUT_INFLATER_SERVICE);
-                        View listItem = inflater.inflate (R.layout.list_item, parent, false);
-                        ImageView img = (ImageView) listItem.findViewById (R.id.imageView1);
-
-                        new DownloadImageTask(img).execute (urls [position]);*/
-                        Log.i ("JENSELTER", urls [position]);
-                        String key = urls [position];
-                        if (getBitmapFromMemCache(key) != null) {
-                            img.setImageBitmap (getBitmapFromMemCache (key));
-                        }
-                        else {
-                            img.setImageDrawable(new ColorDrawable(Color.BLACK));
-                            new DownloadImageTask(img).execute(urls[position]);
-                        }
-
-                        return img;
+                    if (convertView == null) {
+                        img = new ImageView(parent.getContext());
+                        img.setMinimumHeight (150);
+                        img.setPadding(7, 7, 7, 7);
+                    } else {
+                        img = (ImageView) convertView;
                     }
+
+                    /*LayoutInflater inflater = (LayoutInflater) getContext().getSystemService (Context.LAYOUT_INFLATER_SERVICE);
+                    View listItem = inflater.inflate (R.layout.list_item, parent, false);
+                    ImageView img = (ImageView) listItem.findViewById (R.id.imageView1);
+
+                    new DownloadImageTask(img).execute (urls [position]);*/
+                    //Log.i("JENSELTER", "URL: " + urls[position]);
+                    String key = urls[position];
+                    Bitmap b = getBitmapFromMemCache (key);
+                    if (b != null) {
+                        img.setImageBitmap (b);
+                    } else {
+                        if (cancelPotentialDownload (key, img)) {
+                            DownloadImageTask task = new DownloadImageTask (img);
+                            img.setImageDrawable (new DefaultDrawable (task));
+                            task.execute (urls[position]);
+                        }
+                    }
+
+                    return img;
                 }
-            );
+            });
             }
         });
     }
 
+    private boolean cancelPotentialDownload (String url, ImageView imageView) {
+        DownloadImageTask task = getDownloadImageTask (imageView);
+
+        if (task != null) {
+            String imageUrl = task.url;
+            if ((imageUrl == null) || (!imageUrl.equals (url))) {
+                Log.i ("JENSELTER", "Canceling task  for url: " + (imageUrl != null ? imageUrl : "URL Empty!!"));
+                task.cancel (true);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private DownloadImageTask getDownloadImageTask (ImageView imageView) {
+        if (imageView != null) {
+            Drawable d = imageView.getDrawable ();
+            if (d instanceof DefaultDrawable) {
+                return ((DefaultDrawable) d).getDownloadImageTask ();
+            }
+        }
+
+        return null;
+    }
+
     private void addBitmapToMemoryCache (String key, Bitmap bitmap) {
-        if (getBitmapFromMemCache(key) == null) {
+        if (getBitmapFromMemCache(key) != null) {
             memCache.put (key, bitmap);
         }
     }
@@ -126,19 +172,17 @@ public class MainActivity extends Activity {
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewWeakReference;
         private String url;
-        private ImageView img;
-        //private final WeakReference<ImageView> imageViewWeakReference;
 
         public DownloadImageTask (ImageView img) {
-            this.img = img;
-            //imageViewWeakReference = new WeakReference<ImageView>(img);
+            imageViewWeakReference = new WeakReference<ImageView>(img);
         }
 
         @Override
         protected Bitmap doInBackground (String...urls) {
             url = String.valueOf (urls [0]);
-            return downloadBitmap(url);
+            return downloadBitmap (url);
         }
 
         @Override
@@ -147,39 +191,50 @@ public class MainActivity extends Activity {
                 result = null;
             }
 
-            /*if (imageViewWeakReference != null) {
-                ImageView imageView = imageViewWeakReference.get ();
-                if (imageView != null) {
-                    imageView.setImageBitmap (result);
-                }
-            }*/
-
-            addBitmapToMemoryCache (url, result);
-            img.setImageBitmap (result);
+            ImageView imageView = imageViewWeakReference.get ();
+            if (imageView != null && result != null) {
+                imageView.setImageBitmap (result);
+                addBitmapToMemoryCache (url, result);
+            }
 
             super.onPostExecute (result);
         }
 
-        private Bitmap downloadBitmap (String url) {
-            final int IO_BUFFER_SIZE = 1024 * 4;
-            final HttpClient client = AndroidHttpClient.newInstance ("Android");
-            final HttpGet getRequest = new HttpGet (url);
+        private Bitmap downloadBitmap (String uri) {
+            //final int IO_BUFFER_SIZE = 1024 * 4;
+            //final HttpClient client = AndroidHttpClient.newInstance ("Android");
+            //final HttpGet getRequest = new HttpGet (url);
 
             try {
-                HttpResponse response = client.execute (getRequest);
-                final int statusCode = response.getStatusLine().getStatusCode ();
+                URL url = new URL (uri);
+                HttpURLConnection client = (HttpURLConnection) url.openConnection ();
+                //HttpResponse response = client.execute (getRequest);
+                final int statusCode = client.getResponseCode (); //response.getStatusLine().getStatusCode ();
 
-                if (statusCode != HttpStatus.SC_OK) {
+                if (statusCode != 200) {
+                    //.i ("JENSELTER", "Status Error: " + statusCode);
                     return null;
                 }
 
-                final HttpEntity entity  = response.getEntity ();
-                if (entity != null) {
+                //final HttpEntity entity  = response.getEntity ();
+                //if (entity != null) {
                     InputStream inputStream = null;
-                    try {
-                        inputStream = entity.getContent ();
+                    BufferedInputStream buffer;
 
-                        return scaleImage (BitmapFactory.decodeStream (new FilterInputStream (inputStream) {
+                    try {
+                        //inputStream = entity.getContent ();
+                        inputStream = client.getInputStream ();
+                        buffer = new BufferedInputStream (new FlushedInputStream (inputStream));
+                        BitmapFactory.Options options = new BitmapFactory.Options ();
+                        options.inJustDecodeBounds = true;
+                        BitmapFactory.decodeStream (buffer, null, options);
+                        options.inSampleSize = calculateInSampleSize (options, 150, 150);
+                        options.inJustDecodeBounds = false;
+                        //return scaleImage (BitmapFactory.decodeStream (inputStream), 100, 100);
+                        buffer.reset ();
+                        return scaleImage (BitmapFactory.decodeStream (buffer, null, options), 150, 150);
+
+                        /*return scaleImage (BitmapFactory.decodeStream (new FilterInputStream (inputStream) {
                             @Override
                             public long skip (long n) throws IOException {
                                 long totalBytesSkipped = 0L;
@@ -198,30 +253,54 @@ public class MainActivity extends Activity {
 
                                 return totalBytesSkipped;
                             }
-                            }), 100, 100);
+                            }, null, options), 150, 150);*/
 
                     } finally {
                         if (inputStream != null) {
                             inputStream.close ();
+                            inputStream = null;
                         }
 
-                        entity.consumeContent ();
+                        client.disconnect ();
+                        client = null;
+                        //entity.consumeContent ();
                     }
-                }
+                //}
             } catch (IOException e) {
-                getRequest.abort ();
+                //getRequest.abort ();
+                //Log.i ("JENSELTER", "Error Message: " + e.toString ());
             }
             catch (IllegalStateException e) {
-                getRequest.abort ();
+                //getRequest.abort ();
+                //Log.i ("JENSELTER", "Error Message:" + e.toString ());
             }
             catch (Exception e) {
-                getRequest.abort ();
+                //getRequest.abort ();
+                //Log.i ("JENSELTER", "Error Message: " + e.toString ());
             }
             finally {
-                ((AndroidHttpClient) client).close ();
+                //((AndroidHttpClient) client).close ();
             }
 
             return null;
+        }
+
+        private int calculateInSampleSize (BitmapFactory.Options options, int reqWidth, int reqHeight) {
+            final int height = options.outHeight;
+            final int width = options.outWidth;
+            int inSampleSize = 1;
+
+            if (height > reqHeight || width > reqWidth) {
+                final int halfHeight = height / 2;
+                final int halfWidth = width / 2;
+
+                while (((halfHeight / inSampleSize) > reqHeight) && ((halfWidth / inSampleSize) > reqWidth)) {
+                    inSampleSize *= 2;
+                }
+            }
+
+            Log.i ("JENSELTER", String.valueOf (inSampleSize));
+            return inSampleSize;
         }
 
         /*private Bitmap loadImageFromNetwork (String url) {
@@ -263,7 +342,9 @@ public class MainActivity extends Activity {
                 width = (int) (((double) oWIdth / (double) oHeight) * width);
             }
 
-            return Bitmap.createScaledBitmap (bitmap, width, height, false);
+            Bitmap b = Bitmap.createScaledBitmap (bitmap, width, height, true);
+            bitmap = null;
+            return b;
         }
     }
 
@@ -273,6 +354,19 @@ public class MainActivity extends Activity {
         //                !Environment.isExternalStorageRemovable() ? getExternalCacheDir().getPath () :
         //                context.getCacheDir().getPath();
         return null;
+    }
+
+    private class DefaultDrawable extends ColorDrawable {
+        WeakReference<DownloadImageTask> downloadTask;
+
+        public DefaultDrawable (DownloadImageTask task) {
+            super (Color.BLACK);
+            downloadTask = new WeakReference<DownloadImageTask> (task);
+        }
+
+        public DownloadImageTask getDownloadImageTask () {
+            return downloadTask != null ? downloadTask.get () : null;
+        }
     }
 
     /*class ImageBounds {
